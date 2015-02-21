32cb3dfd955ede8fc7ef5a0d16c6ee815254f533
theepicsnail <theepicsnail@gmail.com>
2015-02-20 19:25:07 -0800

    Minor cleanup

--- example/mapresolver.go
+++ example/mapresolver.go
@@ -6,13 +6,12 @@ type MapResolver func([]string) map[string]interface{}
 
 func init() {
 	depgraph.Bind("mapresolver", func() interface{} {
-		var out MapResolver = func(deps []string) map[string]interface{} {
+		return MapResolver(func(deps []string) map[string]interface{} {
 			out := make(map[string]interface{})
 			for i := range deps {
 				out[deps[i]] = depgraph.Resolve(deps[i])
 			}
 			return out
-		}
-		return out
+		})
 	})
 }
